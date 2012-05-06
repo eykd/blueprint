55c4d89bcd50478ae21b77f4680f250b8f0f39e4
deyk <deyk@crossway.org>
2012-05-05 20:34:36 -0700

    Added BlueprintCollection for generating series of blueprints.

--- blueprint/__init__.py
+++ blueprint/__init__.py
@@ -15,9 +15,11 @@ from fields import *
 from mods import *
 from factories import *
 from markov import *
+from collection import *
 
 __all__ = list(base.__all__) \
           + fields.__all__ \
           + mods.__all__ \
           + factories.__all__ \
+          + collection.__all__ \
           + ['base', 'fields', 'taggables', 'factories', 'mods', 'dice']
