8014d846290ff04113622ffd7d7899bf091bfa64
eykd <david@worldsenoughstudios.com>
2025-11-04 23:02:35 -0800

    feat: add runtests.sh script

---
+++ runtests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+set -x
+
+## Automatically reformat code, but ignore breakpoint() and commented code:
+uv run ruff check --fix --unsafe-fixes --ignore T100  --ignore ERA001
+uv run ruff format
+uv run mypy src tests
+uv run  --env-file=.env.test \
+   pytest \
+   -vv \
+   --new-first \
+   --exitfirst \
+   --disable-warnings \
+   --capture=no \
+   -m "not slow" \
+   "$@"
